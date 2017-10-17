a2a0bda66140cc3ace613001db728302290347bd
Marat Ismagilov <maratismagilov@Mac-mini-Marat.local>
2017-10-17 14:12:23 +0300

    Sample Create Account

---
+++ src/main/kotlin/sample/CreateAccount.kt
@@ -0,0 +1,20 @@
+package sample
+
+import TelegraphApi.Telegraph
+import TelegraphApi.TelegraphAccountBuilder
+import TelegraphApi.exceptions.TelegraphException
+
+/**
+ * Created by maratismagilov on 17.10.17.
+ */
+
+
+fun main(args: Array<String>) {
+    try {
+        val account = TelegraphAccountBuilder("Petr", "Petrov", "").build()
+        val telegraph = Telegraph(account)
+        println(account)
+    } catch (ex: TelegraphException) {
+        ex.printStackTrace()
+    }
+}
